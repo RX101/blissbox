f35c1fdd24b6c9f96a073608f39e365d4c6c3ff9
angruixian <15041867@myrp.edu.sg>
2017-12-19 17:20:28 +0800

    Initial Commit

---
+++ Blissbox_new_template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blissbox_new_template
+//
+//  Created by ANG RUI XIAN  on 19/12/17.
+//  Copyright © 2017 ANG RUI XIAN . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
